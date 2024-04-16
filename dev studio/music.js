7608411ff5b55ce7eeb4b646de40485793bb701b
Lexie0Liu <yliu65@inside.artcenter.edu>
2024-04-16 01:25:32 -0700

    add 3 songs

--- dev studio/music.js
+++ dev studio/music.js
@@ -2,16 +2,15 @@
 const songLibrary = [
     { title: "Therefore I Am", artist: "Billie Eilish", cover: "billie.jpeg", file: "path_to_thereforeiam.mp3" },
     { title: "All you had to do was stay", artist: "Taylor Swift", cover: "1989.jpeg", file: "All You Had to Do Was Stay _ 24Naijamuzic.com 1.mp3" },
+    { title: "Lucid Dreams", artist: "Juice WRLD", cover: "luciddream.jpeg", file: "Juice WRLD - Lucid Dreams (Directed by Cole Bennett).mp3"},
     { title: "Kill Bill", artist: "SZA", cover: "killbill.jpeg", file: "SZA - Kill Bill (Audio).mp3" },
+    { title: "Peaches", artist: "ustin Bieber ft. Daniel Caesar & Giveon", cover: "peaches.jpeg", file: "Justin Bieber - Peaches ft. Daniel Caesar, Giveon.mp3" },
     { title: "Blinding Lights", artist: "The Weeknd", cover: "blinding.jpeg", file: "The Weeknd - Blinding Lights (Official Audio).mp3" },
+    { title: "I Don't Wanna Live Forever", artist: "Taylor Swift, ZAYN", cover: "taylor1.jpeg", file: "ZAYN, Taylor Swift - I Dont Wanna Live Forever (Fifty Shades Darker).mp3" },
     { title: "Lover", artist: "Taylor Swift", cover: "lover.jpeg" },
-    { title: "Blinding Lights", artist: "The Weeknd", cover: "blinding.jpeg" },
     { title: "Good 4 You", artist: "Olivia Rodrigo", cover: "good4u.jpeg" },
-    { title: "Peaches", artist: "ustin Bieber ft. Daniel Caesar & Giveon", cover: "peaches.jpeg" },
     { title: "Industry Baby", artist: "Lil Nas X ft. Jack Harlow", cover: "lilnasx.jpeg" },
-    { title: "I Don't Wanna Live Forever", artist: "Taylor Swift", cover: "taylor1.jpeg" },
     { title: "SOUR CANDY", artist: "Lady Gaga & BLACKPINK", cover: "sourcandy.jpeg" },
-    { title: "Lucid Dreams", artist: "Juice WRLD", cover: "luciddream.jpeg" },
     // add..
     // 省略其他歌曲，确保添加完整信息...
 ];
