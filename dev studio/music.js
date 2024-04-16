b947a8e172d4f10b3833e5b5a5bcaef2dede99d8
Lexie0Liu <yliu65@inside.artcenter.edu>
2024-04-16 01:08:18 -0700

    play music version
    edit version

--- dev studio/music.js
+++ dev studio/music.js
@@ -1,9 +1,9 @@
 // music
 const songLibrary = [
-    { title: "Kill Bill", artist: "SZA", cover: "killbill.jpeg" },
-    // { title: "As It Was", artist: "Harry Styles", cover: "asitwas.jpeg" },
-    { title: "All you had to do was stay", artist: "Taylor Swift", cover: "1989.jpeg" },
-    { title: "Therefore I Am", artist: "Billie Eilish", cover: "billie.jpeg" },
+    { title: "Therefore I Am", artist: "Billie Eilish", cover: "billie.jpeg", file: "path_to_thereforeiam.mp3" },
+    { title: "All you had to do was stay", artist: "Taylor Swift", cover: "1989.jpeg", file: "All You Had to Do Was Stay _ 24Naijamuzic.com 1.mp3" },
+    { title: "Kill Bill", artist: "SZA", cover: "killbill.jpeg", file: "SZA - Kill Bill (Audio).mp3" },
+    { title: "Blinding Lights", artist: "The Weeknd", cover: "blinding.jpeg", file: "The Weeknd - Blinding Lights (Official Audio).mp3" },
     { title: "Lover", artist: "Taylor Swift", cover: "lover.jpeg" },
     { title: "Blinding Lights", artist: "The Weeknd", cover: "blinding.jpeg" },
     { title: "Good 4 You", artist: "Olivia Rodrigo", cover: "good4u.jpeg" },
@@ -13,19 +13,27 @@ const songLibrary = [
     { title: "SOUR CANDY", artist: "Lady Gaga & BLACKPINK", cover: "sourcandy.jpeg" },
     { title: "Lucid Dreams", artist: "Juice WRLD", cover: "luciddream.jpeg" },
     // add..
+    // 省略其他歌曲，确保添加完整信息...
 ];
+
 
 let currentSongIndex = 0;
 
 function playNextSong() {
-    
     currentSongIndex = (currentSongIndex + 1) % songLibrary.length;
     const currentSong = songLibrary[currentSongIndex];
-    // refresh
+    
+    // 更新歌曲信息
     document.getElementById('song-title').innerText = currentSong.title;
     document.getElementById('artist-name').innerText = `by ${currentSong.artist}`;
     document.getElementById('album-cover').src = currentSong.cover;
+
+    // 设置并播放音频
+    const audioPlayer = document.getElementById('audio-player');
+    audioPlayer.src = currentSong.file;
+    audioPlayer.play();
 }
+
 
 document.getElementById('next-song').addEventListener('click', playNextSong);
 
